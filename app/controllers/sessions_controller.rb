0b6d61b75cd86afa2c61714480b968884194efc4
Diego Noronha <dbs.noronha@gmail.com>
2025-04-10 18:33:13 -0400

    [REFAC] Update SessionsController to standardize user_params and improve session creation logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,23 @@
 class SessionsController < ApplicationController
-  skip_before_action :require_authentication, only: [:create]
-
+  skip_before_action :require_authentication, only: [ :create ]
+  
   def create
-    if user = User.authenticate_by(email: params[:email], password: params[:password])
-      session = user.sessions.create!(
-        user_agent: request.user_agent,
-        ip_address: request.ip
-      )
+    if user = User.authenticate_by(email_address: user_params[:email_address], password: user_params[:password])
+      session = user.sessions.create!(user_agent: request.user_agent, ip_address: request.remote_ip)
       render json: { token: session.token }, status: :created
     else
-      render json: { error: I18n.t('authentication.errors.invalid_credentials') }, status: :unauthorized
+      render json: { error: I18n.t("authentication.errors.invalid_credentials") }, status: :unauthorized
     end
   end
-  
+
   def destroy
-    Current.session&.destroy
+    Current.session.destroy
     head :no_content
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email_address, :password)
   end
 end
