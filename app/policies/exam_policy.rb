bec5872630baf4087bfdcf56831b6a75b62db0a3
Diego Noronha <dbs.noronha@gmail.com>
2025-04-13 16:26:58 -0400

    [FEATURE] Create ExamPolicy class to manage permissions for exam actions based on user roles

---
+++ app/policies/exam_policy.rb
@@ -0,0 +1,38 @@
+class ExamPolicy < ApplicationPolicy
+  def index?
+    user.admin? || user.professional? || user.patient?
+  end
+
+  def show?
+    user.admin? || 
+    (user.professional? && record.professional_id == user.id) || 
+    (user.patient? && record.patient_id == user.id)
+  end
+
+  def create?
+    user.admin? || user.professional?
+  end
+
+  def update?
+    user.admin? || 
+    (user.professional? && record.professional_id == user.id)
+  end
+
+  def destroy?
+    user.admin?
+  end
+
+  class Scope < Scope
+    def resolve
+      if user.admin?
+        scope.all
+      elsif user.professional?
+        scope.where(professional_id: user.id)
+      elsif user.patient?
+        scope.where(patient_id: user.id)
+      else
+        scope.none
+      end
+    end
+  end
+end 
