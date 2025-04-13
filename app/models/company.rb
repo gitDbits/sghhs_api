486baf5b3c1cacd6fdf42eb68186ab02be8f5658
Diego Noronha <dbs.noronha@gmail.com>
2025-04-13 16:23:09 -0400

    [FEATURE] Add Company model with auditing for specified attributes

---
+++ app/models/company.rb
@@ -0,0 +1,3 @@
+class Company < ApplicationRecord
+  audited only: [:cnpj, :email, :name, :fantasy_name, :address, :address_number, :district, :city_id, :state_id, :zipcode, :phone, :company_type_id]
+end
