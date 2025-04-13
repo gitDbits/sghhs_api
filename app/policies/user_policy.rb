3d6bd968279ddb17cd32ac5dfc944354e9fe45dd
Diego Noronha <dbs.noronha@gmail.com>
2025-04-13 16:27:48 -0400

    [FEATURE] Create UserPolicy class to manage permissions for user actions, restricting access to admin users and allowing users to manage their own records

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,33 @@
+class UserPolicy < ApplicationPolicy
+  def index?
+    user.admin?
+  end
+
+  def show?
+    user.admin? || record.id == user.id
+  end
+
+  def create?
+    # Only admins can create users
+    user.admin?
+  end
+
+  def update?
+    user.admin? || record.id == user.id
+  end
+
+  def destroy?
+    user.admin?
+  end
+
+  class Scope < Scope
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        # Non-admins can only see themselves
+        scope.where(id: user.id)
+      end
+    end
+  end
+end 
