cdd945efb2715b61da3c680fd3cb1c415c190941
Diego Noronha <dbs.noronha@gmail.com>
2025-04-13 16:24:39 -0400

    [FEATURE] Add Role model with user associations and validations

---
+++ app/models/role.rb
@@ -0,0 +1,14 @@
+class Role < ApplicationRecord
+  has_many :user_roles, dependent: :destroy
+  has_many :users, through: :user_roles
+
+  validates :name, presence: true, uniqueness: true
+
+  def self.admin
+    find_by(name: 'admin')
+  end
+
+  def self.user
+    find_by(name: 'user')
+  end
+end
