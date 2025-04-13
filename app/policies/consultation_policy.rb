accbef5b7a56cd172695d19280452c5aa13a8920
Diego Noronha <dbs.noronha@gmail.com>
2025-04-13 16:26:46 -0400

    [FEATURE] Create ConsultationPolicy class to manage permissions for consultation actions based on user roles

---
+++ app/policies/consultation_policy.rb
@@ -0,0 +1,35 @@
+class ConsultationPolicy < ApplicationPolicy
+  def index?
+    user.admin? || user.professional? || user.patient?
+  end
+
+  def show?
+    return true if user.admin?
+    return true if user.professional? && record.professional_id == user.id
+    return true if user.patient? && record.patient_id == user.id
+
+    false
+  end
+
+  def create?
+    user.admin? || user.professional?
+  end
+
+  def update?
+    user.admin? || (user.professional? && record.professional_id == user.id)
+  end
+
+  def destroy?
+    user.admin?
+  end
+
+  class Scope < Scope
+    def resolve
+      return scope.all if user.admin?
+      return scope.where(professional_id: user.id) if user.professional?
+      return scope.where(patient_id: user.id) if user.patient?
+
+      scope.none
+    end
+  end
+end
