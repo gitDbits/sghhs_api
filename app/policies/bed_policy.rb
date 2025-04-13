e007431c027ce1b5a5ea8ef82cf9b180f5db1043
Diego Noronha <dbs.noronha@gmail.com>
2025-04-13 16:26:07 -0400

    [FEATURE] Create BedPolicy class to manage permissions for bed-related actions based on user roles

---
+++ app/policies/bed_policy.rb
@@ -0,0 +1,31 @@
+class BedPolicy < ApplicationPolicy
+  def index?
+    user.admin?
+  end
+
+  def show?
+    user.admin?
+  end
+
+  def create?
+    user.admin?
+  end
+
+  def update?
+    user.admin?
+  end
+
+  def destroy?
+    user.admin?
+  end
+
+  class Scope < Scope
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        scope.where(room_id: user.room_id)
+      end
+    end
+  end
+end 
