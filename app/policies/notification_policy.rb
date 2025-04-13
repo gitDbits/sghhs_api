aed1bd8ba8707bbfff835a2a6e29212388f0c972
Diego Noronha <dbs.noronha@gmail.com>
2025-04-13 16:27:21 -0400

    [FEATURE] Create NotificationPolicy class to manage permissions for notification actions based on user roles

---
+++ app/policies/notification_policy.rb
@@ -0,0 +1,35 @@
+class NotificationPolicy < ApplicationPolicy
+  def index?
+    true
+  end
+
+  def show?
+    user.admin? || record.user_id == user.id
+  end
+
+  def create?
+    user.admin?
+  end
+
+  def update?
+    user.admin?
+  end
+
+  def destroy?
+    user.admin? || record.user_id == user.id
+  end
+
+  def mark_as_read?
+    user.admin? || record.user_id == user.id
+  end
+
+  class Scope < Scope
+    def resolve
+      if user.admin?
+        scope.all
+      else
+        scope.where(user_id: user.id)
+      end
+    end
+  end
+end 
