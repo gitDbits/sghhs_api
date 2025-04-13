0b45ca27ed9840e85ba5137f88338dd6762cf319
Diego Noronha <dbs.noronha@gmail.com>
2025-04-13 16:29:29 -0400

    [FEATURE] Create migration for roles table with name and description fields

---
+++ db/migrate/20250413180835_create_roles.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateRoles < ActiveRecord::Migration[8.0]
+  def change
+    create_table :roles do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
