bb66719f9e7f7f243a7e015a1b3822afc705110b
Diego Noronha <dbs.noronha@gmail.com>
2025-04-13 16:28:18 -0400

    [FEATURE] Add routes for bed management, company management, and geographical entities

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
+  resources :bed_types
+  resources :beds
+  resources :companies
+  resources :company_types
+  resources :states
+  resources :cities  
 end
